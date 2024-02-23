32e26f012e71fe8a939b3c1dca97e0486458f93c
Felekyu <flo.hibois.dev@gmail.com>
2024-02-23 12:31:29 +0100

    Added JavaDoc

--- src/main/java/config/PropertiesFile.java
+++ src/main/java/config/PropertiesFile.java
@@ -6,16 +6,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * Loads the properties file and stores its content
+ * @author felek
+ */
 public class PropertiesFile
 {
 	private static final Properties prop = new Properties();
 
+	/**
+	 * Retrieves property value by its name
+	 * @param propertyName the property name
+	 * @return the value for the specified property
+	 */
 	public static String getProperty(String propertyName)
 	{
 		String property = prop.getProperty(propertyName);
 		return property.trim().isEmpty() ? null : property.trim();
 	}
 
+	/**
+	 * Reads and loads the properties file into the class
+	 */
 	public static void loadProperties()
 	{
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
