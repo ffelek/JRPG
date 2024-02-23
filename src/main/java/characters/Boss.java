d0dd8fd18863a3abbcf5e4fd846fd1194c1c27f5
Felekyu <flo.hibois.dev@gmail.com>
2024-02-23 12:28:43 +0100

    Added JavaDoc

--- src/main/java/characters/Boss.java
+++ src/main/java/characters/Boss.java
@@ -4,6 +4,10 @@ import equipments.Weapon;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Defines a boss
+ * @author felek
+ */
 @Getter
 @Setter
 public class Boss extends ACharacterAttributes
@@ -11,6 +15,16 @@ public class Boss extends ACharacterAttributes
 	private int level;
 	private Weapon activeWeapon;
 
+	/**
+	 * Constructor for a boss
+	 * @param id id
+	 * @param level level
+	 * @param name name
+	 * @param nickname nickname
+	 * @param hp Health Points
+	 * @param mp Mana Points
+	 * @param stamina Stamina Points
+	 */
 	public Boss(int id, int level, String name, String nickname, int hp, int mp, int stamina)
 	{
 		super(id, name, nickname, hp, mp, stamina);
--- src/main/java/characters/Character.java
+++ src/main/java/characters/Character.java
@@ -5,6 +5,10 @@ import lombok.Setter;
 import equipments.Equipment;
 import java.util.List;
 
+/**
+ * Defines a playable character
+ * @author felek
+ */
 @Getter
 @Setter
 public final class Character extends ACharacterAttributes
@@ -12,6 +16,15 @@ public final class Character extends ACharacterAttributes
 	private int weight;
 	private List<Equipment> equipments;
 
+	/**
+	 * Constructor for character (player)
+	 * @param id id
+	 * @param name name
+	 * @param hp Health Points
+	 * @param mp Mana Points
+	 * @param stamina Stamina Points
+	 * @param weight Weight Capacity
+	 */
 	public Character(int id, String name, int hp, int mp, int stamina, int weight)
 	{
 		super(id, name, "", hp, mp, stamina);
