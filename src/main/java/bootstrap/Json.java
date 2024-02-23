268a1baf922c26f6e4832a3d79c33c102c9edab9
Felekyu <flo.hibois.dev@gmail.com>
2024-02-23 11:49:29 +0100

    Added JavaDoc Adjusted the type thrown in loadData()

--- src/main/java/bootstrap/Json.java
+++ src/main/java/bootstrap/Json.java
@@ -5,20 +5,28 @@ import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 import utils.Constants;
 import config.PropertiesFile;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
+/**
+ * Class loading the JSON source file and parsing it
+ * @author felek
+ */
 public class Json
 {
 	public static JsonObject data = new JsonObject();
 
-	public static void loadData() throws IOException
+	/**
+	 * Retrieves path from properties file and read the file to parse its content into JsonObject
+	 * @throws FileNotFoundException if file is not found
+	 */
+	public static void loadData() throws FileNotFoundException
 	{
 		String s = PropertiesFile.getProperty(Constants.CONFIG_FILE_ACCESS_KEY);
 		if (s != null && !s.isEmpty())
 		{
-			Gson gson = new Gson();
 			JsonReader reader = new JsonReader(new FileReader(s));
+			Gson gson = new Gson();
 			data = gson.fromJson(reader, JsonObject.class);
 		}
 	}
